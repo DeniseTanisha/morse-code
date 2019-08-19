1be54d82935bfd9185036bb84579f652b38b72f2
Denise Stubbs <denise.stubbs@Nology.local>
2019-08-19 17:57:56 +0100

    start of functions

--- main.js
+++ main.js
@@ -1,6 +1,79 @@
-function sum(a, b) {
-  return a + b;
-}
+const morseKey = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "0": "-----",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "'": ".----.",
+  "!": "-.-.--",
+  "/": "-..-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  "&": ".-...",
+  ":": "---...",
+  ";": "-.-.-.",
+  "=": "-...-",
+  "+": ".-.-.",
+  "-": "-....-",
+  _: "..--.-",
+  '"': ".-..-.",
+  $: "...-..-",
+  "@": ".--.-.",
+  " ": "/"
+};
+
+// words to morse
+
+decodeMorse = function(morseCode) {
+  function decodeMorseLetter(Letter) {
+    return morseKey[letter];
+  }
+  function decodeMorseWord(word) {
+    return word
+      .split("")
+      .map(decodeMorseLetter)
+      .join("");
+  }
+  return morseCode
+    .trim()
+    .split(" ")
+    .map(decodeMorseWord)
+    .join("");
+};
 
 function conv() {
   var input = "Hello World";
@@ -12,5 +85,3 @@ function conv() {
 }
 
 module.exports = conv;
-
-module.exports = sum;
